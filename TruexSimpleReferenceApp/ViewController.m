7f9b4418cc5cbb3aab0b9103c9147b1b5005c671
Andrew Tran <andrew.tran@infillion.com>
2024-08-13 10:27:46 -0700

    test: update vastConfigUrl to prod intel ad

--- TruexSimpleReferenceApp/ViewController.m
+++ TruexSimpleReferenceApp/ViewController.m
@@ -67,7 +67,7 @@ static int const MidrollAdBreakDimensionValue = 2;
     NSString *userId = [NSUUID UUID].UUIDString;
     
     // final string should format to (network_user_id parameter will change value each time):
-    NSString *innovidTvosVastConfigUrl = [NSString stringWithFormat:@"https://qa-get.truex.com/bdfe2ba97e74172a75e325d307db6cfc16f92325/vast/config?dimension_1=test&dimension_2=%d&stream_position=%@ network_user_id=%@",
+    NSString *innovidTvosVastConfigUrl = [NSString stringWithFormat:@"https://get.truex.com/bdfe2ba97e74172a75e325d307db6cfc16f92325/vast/config?dimension_1=test&dimension_2=%d&stream_position=%@ network_user_id=%@",
                                MidrollAdBreakDimensionValue,
                                MIDROLL,
                                userId];
